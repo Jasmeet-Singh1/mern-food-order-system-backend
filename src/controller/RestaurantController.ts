9852649de0358e9185062b9b5ac74d371153b81f
my-name <jassoffline586@gmail.com>
2024-05-28 19:04:19 -0700

    Added checkout flow

--- src/controller/RestaurantController.ts
+++ src/controller/RestaurantController.ts
@@ -1,6 +1,21 @@
 import { Request, Response } from 'express';
 import Restaurant from '../models/restaurant';
 
+const getRestaurant = async (req: Request, res: Response) => {
+  try {
+    const restaurantId = req.params.restaurantId;
+
+    const restaurant = await Restaurant.findById(restaurantId);
+    if (!restaurant) {
+      return res.status(404).json({ message: 'restaurant not found' });
+    }
+
+    res.json(restaurant);
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong' });
+  }
+};
+
 const searchRestaurant = async (req: Request, res: Response) => {
   try {
     const city = req.params.city;
@@ -66,4 +81,5 @@ const searchRestaurant = async (req: Request, res: Response) => {
 
 export default {
   searchRestaurant,
+  getRestaurant,
 };
--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -3,7 +3,11 @@ import { param } from 'express-validator';
 import RestaurantController from '../controller/RestaurantController';
 
 const router = express.Router();
-
+router.get(
+  '/:restaurantId',
+  param('restaurantId').isString().trim().notEmpty().withMessage('RestaurantId paramenter must be a valid string'),
+  RestaurantController.getRestaurant
+);
 router.get(
   '/search/:city',
   param('city').isString().trim().notEmpty().withMessage('City paramenter must be a valid string'),
