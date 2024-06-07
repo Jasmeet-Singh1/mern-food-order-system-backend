de6f6e846b7ee3deb5384fa513cb3574e0ba544d
my-name <jassoffline586@gmail.com>
2024-06-07 15:17:10 -0700

    final commit

--- src/routes/MyRestaurantRoute.ts
+++ src/routes/MyRestaurantRoute.ts
@@ -10,18 +10,15 @@ const storage = multer.memoryStorage();
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 5 * 1024 * 1024, // 5mb
+    fileSize: 5 * 1024 * 1024,
   },
 });
-// Get /api/my/restaurant
 
 router.get('/order', jwtCheck, jwtParse, MyRestaurantController.getMyRestaurantOrders);
 
 router.patch('/order/:orderId/status', jwtCheck, jwtParse, MyRestaurantController.updateOrderStatus);
 
 router.get('/', jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
-
-// Get /api/my/restaurant
 
 router.post(
   '/',
